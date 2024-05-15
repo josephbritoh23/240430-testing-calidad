7d4078eb9d89ac9e72ad76b58ad1d9a95612cc69
josephbritohenao <105831821+josephbritohenao@users.noreply.github.com>
2024-05-14 21:39:23 -0500

    prueba

--- src/01-sum/02-sub/sub.js
+++ src/01-sum/02-sub/sub.js
@@ -1,9 +1,10 @@
 const sub = (num1, num2) => {
-    if (typeof num1 !== "number" || typeof num2 !== "number") {
-      return "los argumentos deben ser numeros enteros";
-    }
-    return Math.abs(num1 - num2);
-  };
-  
-  module.exports = sub;
+  if (typeof num1 !== "number" || typeof num2 !== "number") {
+    return "los argumentos deben ser numeros enteros";
+  }
+  const result = Math.abs(num1 - num2);
+  return result >= 100 ? "la resta sobrepasó los limites" : result;
+};
+
+module.exports = sub;
   
